c67e148ad4d995cbeb25a8fa340be9b953ba9f3f
arkeshdas <dasarkes@msu.edu>
2026-02-23 17:54:24 -0500

    Restructure repo to new pipeline layout

---
+++ julia/src/test_import.jl
@@ -0,0 +1,9 @@
+using CurricularAnalytics
+using CSV
+using DataFrames
+
+println("CurricularAnalytics loaded successfully.")
+
+# dummy DataFrame test
+df = DataFrame(course = ["MTH 101", "MTH 102"])
+println(df)
