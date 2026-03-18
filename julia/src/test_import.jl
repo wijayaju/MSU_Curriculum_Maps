3ddd067c4ae56f1e14faeeb3ae4667361f70df0c
arkeshdas <dasarkes@msu.edu>
2026-03-18 19:56:18 -0400

    removed the julia folder

--- julia/src/test_import.jl
+++
@@ -1,9 +0,0 @@
-using CurricularAnalytics
-using CSV
-using DataFrames
-
-println("CurricularAnalytics loaded successfully.")
-
-# dummy DataFrame test
-df = DataFrame(course = ["MTH 101", "MTH 102"])
-println(df)
